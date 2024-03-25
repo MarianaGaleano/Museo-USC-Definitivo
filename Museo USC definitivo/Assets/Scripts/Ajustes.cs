2bb00e7fdab34653ef8bc4c661a604e46e45fcae
JuanR <112212338+JuanPython1@users.noreply.github.com>
2024-03-25 18:10:57 -0500

    Botón Reproducir Audios

---
+++ Museo USC definitivo/Assets/Scripts/Ajustes.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ajustes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Oculta el puntero del mouse al iniciar el juego
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Museo USC definitivo/Assets/Scripts/Punto De Mira.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PuntoDeMira : MonoBehaviour
+{
+    public Camera cam;
+    public AudioSource audioSource;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit) && hit.transform == this.transform)
+            {
+                if (audioSource.isPlaying)
+                {
+                    audioSource.Pause();
+                }
+                else
+                {
+                    audioSource.Play();
+                }
+            }
+        }
+    }
+}
